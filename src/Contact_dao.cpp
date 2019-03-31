17b2aaffc61c8bdc089996585d246387589f5a15
Chrinkus <seeschickrun@gmail.com>
2019-03-30 23:34:56 -0500

    Completed CRUD ops for dao.

--- src/Contact_dao.cpp
+++ src/Contact_dao.cpp
@@ -12,7 +12,7 @@ Contact_dao::Contact_dao(Sxy::Database& database)
 void Contact_dao::init() const
 {
     if (!db.has_table("contacts")) {
-        Sxy::Query query{db};
+        Sxy::Query query {db};
         query.exec("CREATE TABLE contacts ("
                 "id INTEGER PRIMARY KEY AUTOINCREMENT, "
                 "first_name TEXT, "
@@ -25,7 +25,7 @@ void Contact_dao::init() const
 
 bool Contact_dao::create_contact(Contact& contact) const
 {
-    Sxy::Query query{db};
+    Sxy::Query query {db};
     query.prepare("INSERT INTO contacts "
             "(first_name, last_name, birthday, email, phone_number) "
             "VALUES "
@@ -38,4 +38,49 @@ bool Contact_dao::create_contact(Contact& contact) const
     bool res = query.exec();
     contact.id(static_cast<int>(query.last_insert_id()));
     return res;
+}
+
+bool Contact_dao::delete_contact(int id) const
+{
+    Sxy::Query query {db};
+    query.prepare("DELETE FROM contacts WHERE id=:id;");
+    query.bind_value(":id", id);
+    return query.exec();
+}
+
+bool Contact_dao::update_contact(const Contact& contact) const
+{
+    Sxy::Query query {db};
+    query.prepare("UPDATE contacts SET "
+            "first_name = :first_name, "
+            "last_name = :last_name, "
+            "birthday = :birthday, "
+            "email = :email, "
+            "phone_number = :phone_number "
+            "WHERE id = :id;");
+    query.bind_value(":first_name", contact.first());
+    query.bind_value(":last_name", contact.last());
+    query.bind_value(":birthday", contact.birth());
+    query.bind_value(":email", contact.email());
+    query.bind_value(":phone_number", contact.phone());
+    query.bind_value(":id", contact.id());
+    return query.exec();
+}
+
+std::vector<std::unique_ptr<Contact>> Contact_dao::contacts() const
+{
+    std::vector<std::unique_ptr<Contact>> vpc;
+    Sxy::Query query {db};
+    query.prepare("SELECT * FROM contacts;");
+    while (query.step()) {
+        auto pcontact = std::make_unique<Contact>();
+        pcontact->id(query.value("id").to_int());
+        pcontact->first(query.value("first_name").to_string());
+        pcontact->last(query.value("last_name").to_string());
+        pcontact->birth(query.value("birthday").to_string());
+        pcontact->email(query.value("email").to_string());
+        pcontact->phone(query.value("phone_number").to_string());
+        vpc.push_back(std::move(pcontact));
+    }
+    return vpc;
 }
--- test/tests-Contact_dao.cpp
+++ test/tests-Contact_dao.cpp
@@ -2,7 +2,10 @@
 
 #include "Contact_dao.hpp"
 
+#include "Contact.hpp"
 #include <shixxy/Sxy_Database.hpp>
+
+#include <algorithm>
 
 TEST_CASE("Contact_dao interacts with Sxy::Database", "[Contact_dao]") {
     Sxy::Database db;
@@ -12,4 +15,46 @@ TEST_CASE("Contact_dao interacts with Sxy::Database", "[Contact_dao]") {
 
     dao.init();
     REQUIRE(db.has_table("contacts") == true);
+
+    SECTION("Contact_dao:: CRUD operations work") {
+
+        Contact pam { "Pam", "Beesly", "1974-03-07" };
+        Contact jim { "Jim", "Halpert", "1979-10-20" };
+        Contact dwight { "Dwight", "Schrute", "1966-01-20" };
+
+        dao.create_contact(pam);
+        dao.create_contact(jim);
+        dao.create_contact(dwight);
+
+        REQUIRE(pam.id() == 1);
+        REQUIRE(jim.id() == 2);
+        REQUIRE(dwight.id() == 3);
+
+        auto vcontacts = dao.contacts();
+        REQUIRE(vcontacts.size() == 3);
+
+        auto it = std::find_if(std::begin(vcontacts), std::end(vcontacts),
+                [](const auto& pc) { return pc->first() == "Jim"; });
+        REQUIRE(it != std::end(vcontacts));
+
+        dao.delete_contact((*it)->id());
+        vcontacts = dao.contacts();
+        REQUIRE(vcontacts.size() == 2);
+        
+        it = std::find_if(std::begin(vcontacts), std::end(vcontacts),
+                [](const auto& pc) { return pc->first() == "Jim"; });
+        REQUIRE(it == std::end(vcontacts));
+
+        REQUIRE(vcontacts.front()->last() == "Beesly");
+        REQUIRE(vcontacts.front()->email().empty() == true);
+        REQUIRE(vcontacts.front()->phone().empty() == true);
+        pam.last("Halpert");
+        pam.email("pam@dundermifflin.com");
+        pam.phone("(999) 555-5678");
+        dao.update_contact(pam);
+        vcontacts = dao.contacts();
+        REQUIRE(vcontacts.front()->last() == "Halpert");
+        REQUIRE(vcontacts.front()->email() == "pam@dundermifflin.com");
+        REQUIRE(vcontacts.front()->phone() == "(999) 555-5678");
+    }
 }
